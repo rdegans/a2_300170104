9e1d55788b48f66cb6be115ef7be61cc8af23929
Kcezirim <79070629+Kcezirim@users.noreply.github.com>
2021-02-17 21:57:19 -0500

    update

--- AttributeType.java
+++ AttributeType.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * Enumeration class for attribute types
+ * 
+ * @author Mehrdad Sabetzadeh, University of Ottawa
+ * @author Guy-Vincent Jourdan, University of Ottawa
  *
- * @author degan
  */
-public class AttributeType {
-    
+public enum AttributeType {
+	NOMINAL, NUMERIC
 }
